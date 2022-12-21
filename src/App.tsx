bec49270cad3b55b53a9ac211470eb199a9c18bc
thejal-amy <thejal.amy@gmail.com>
2022-12-21 13:57:34 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./App.css";
+import BoardList from "./BoardList";
+import Write from "./Write";
+
+function App() {
+  return (
+    <div className="App">
+      <BoardList></BoardList>
+      <Write></Write>
+    </div>
+  );
+}
+
+export default App;
